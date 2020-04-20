afa153b7c6cb02d6f8c1562185ad354e354b0832
Martinez Jurado, Victor <victor.martinez@sicpa.com>
2020-04-20 21:06:05 +0200

    add sicpa vendor

--- plugfest-2020/vendors/index.js
+++ plugfest-2020/vendors/index.js
@@ -3,6 +3,7 @@ const digital_bazaar = require("./digital_bazaar");
 const danubetech = require("./danubetech");
 const mavennet = require("./mavennet");
 const trustbloc = require("./trustbloc");
+const sicpa = require("./sicpa");
 
 let vendor_map = {
   transmute,
@@ -10,6 +11,7 @@ let vendor_map = {
   danubetech,
   mavennet,
   trustbloc,
+  sicpa,
 };
 
 let vendors = [];
